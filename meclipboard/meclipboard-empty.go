153e863985f8cbf88044bc7b8dae938cd1c2e722
zanjie1999 <zanjie1999@yeah.net>
2024-02-25 01:51:32 +0800

    OnChange

--- meclipboard/meclipboard-empty.go
+++ meclipboard/meclipboard-empty.go
@@ -4,16 +4,18 @@ package meclipboard
 
 type MeClipboardService struct{}
 
-func MeClipboard() *MeClipboardService                                    { return nil }
-func (c *MeClipboardService) Watch() chan bool                            { return nil }
-func (c *MeClipboardService) Clear() error                                { return nil }
-func (c *MeClipboardService) ContainsText() (available bool, err error)   { return false, nil }
-func (c *MeClipboardService) ContainsFile() (available bool, err error)   { return false, nil }
-func (c *MeClipboardService) ContainsBitmap() (available bool, err error) { return false, nil }
-func (c *MeClipboardService) ContentType() (string, error)                { return "", nil }
-func (c *MeClipboardService) Text() (text string, err error)              { return "", nil }
-func (c *MeClipboardService) Bitmap() (bmpBytes []byte, err error)        { return nil, nil }
-func (c *MeClipboardService) Files() (filenames []string, err error)      { return nil, nil }
-func (c *MeClipboardService) SetText(s string) error                      { return nil }
-func (c *MeClipboardService) SetFiles(paths []string) error               { return nil }
-func (c *MeClipboardService) withOpenClipboard(f func() error) error      { return nil }
+func MeClipboard() *MeClipboardService                                       { return nil }
+func (c *MeClipboardService) Watch() chan bool                               { return nil }
+func (c *MeClipboardService) Clear() error                                   { return nil }
+func (c *MeClipboardService) ContainsText() (available bool, err error)      { return false, nil }
+func (c *MeClipboardService) ContainsFile() (available bool, err error)      { return false, nil }
+func (c *MeClipboardService) ContainsBitmap() (available bool, err error)    { return false, nil }
+func (c *MeClipboardService) ContentType() (string, error)                   { return "", nil }
+func (c *MeClipboardService) Text() (text string, err error)                 { return "", nil }
+func (c *MeClipboardService) Bitmap() (bmpBytes []byte, err error)           { return nil, nil }
+func (c *MeClipboardService) Files() (filenames []string, err error)         { return nil, nil }
+func (c *MeClipboardService) BitmapOnChange() (bmpBytes []byte, err error)   { return nil, nil }
+func (c *MeClipboardService) FilesOnChange() (filenames []string, err error) { return nil, nil }
+func (c *MeClipboardService) SetText(s string) error                         { return nil }
+func (c *MeClipboardService) SetFiles(paths []string) error                  { return nil }
+func (c *MeClipboardService) withOpenClipboard(f func() error) error         { return nil }
